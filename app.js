3012220d1bc56cb856eddab0c666be5ed62ed990
fritzimperial <fimperial@ncf.edu.ph>
2024-10-01 09:35:46 +0800

    first version

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Routes Here
+const authRoutes = require('./routes/authRoutes');
+const userRoute = require('./routes/userRoute');
+
+const app = express();
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', function(req, res){
+    res.send("Fritz Imperial, MIT");
+});
+
+// Endpoint Here
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoute);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
